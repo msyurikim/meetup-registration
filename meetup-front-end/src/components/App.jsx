546f6664d13c0ae1820d3649399e8eb9bb98512e
Jesse Beard <jjbeard@protonmail.com>
2020-05-18 09:42:38 -0700

    added state and input handling

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -5,15 +5,42 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      atendees: []
+      atendees: [],
+      firstName: '',
+      lastName: '',
+      email: '',
+      shirtSize: '', // XS, S, M, L, XL, XXL
+      experienceLevel: '' // beginner, intermediate, expert
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
 
 
   render(){
     return (
-      <h2>New Attendee</h2>
+      <div style={{ width: '50%' }}>
+        <h2>New Attendee</h2>
+        <form>
+          <label>
+            First name:
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.value}
+              onChange={this.handleInputChange}
+            />
+          </label>
+        </form>
+      </div>
     )
   }
 
