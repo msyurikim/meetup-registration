542634b34441a9a17dfd74263982000c8d619712
melvinioso <matt.mcgavern@gmail.com>
2020-05-18 11:55:30 -0500

    Added constructor, componentDidMount, and render function

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -3,7 +3,33 @@ import React from 'react';
 
 class App extends React.Component {
 // YOUR CODE HERE
+  constructor(props) {
+    super(props);
+    this.state = {
+      attendees: [],
+    }
+  }
 
+  componentDidMount() {
+    axios.get('/attendees')
+      .then((res) => {
+        this.setState({
+          attendees: res.data,
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="main">
+        <Attendees attendees={this.state.attendees} />
+      </div>
+    );
+  }
 }
 
 export default App;
