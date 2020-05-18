ef3470077533c35f8d7e54920427e5670d264c4d
joshskkim <josh.sk.kim@gmail.com>
2020-05-18 09:18:49 -0700

    Makes get request to server

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import axios from 'axios';
 
 class App extends React.Component {
-// YOUR CODE HERE
+  // YOUR CODE HERE
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    }
+  }
+
+  componentDidMount() {
+    this.getAttendees();
+  }
+
+  getAttendees() {
+    axios.get('/attendees')
+      .then(results => {
+        console.log(results);
+      });
+  }
+
+  render () {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
 }
 
 export default App;
