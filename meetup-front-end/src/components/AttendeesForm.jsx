4a7f767a804687e5027a493613cc2236b0761f84
joshskkim <josh.sk.kim@gmail.com>
2020-05-18 09:50:54 -0700

    Added options for shirt size and experience level

--- meetup-front-end/src/components/AttendeesForm.jsx
+++ meetup-front-end/src/components/AttendeesForm.jsx
@@ -20,14 +20,28 @@ class AttendeesForm extends React.Component {
           <input type="text"></input>
         </label>
         <label>Email Address:
-          <input type="text"></input>
+          <input type="email"></input>
         </label>
-        <label>Shirt Size:
-          <input type="text"></input>
-        </label>
-        <label>Experience Level:
-          <input type="text"></input>
-        </label>
+        <form>Shirt Size:
+          <input type="radio" id="XS" name="shirt size" value ="XS" />
+          <label htmlFor="XS">XS</label><br />
+          <input type="radio" id="S" name="shirt size" value ="S" />
+          <label htmlFor="S">S</label><br />
+          <input type="radio" id="M" name="shirt size" value ="M" />
+          <label htmlFor="M">M</label><br />
+          <input type="radio" id="L" name="shirt size" value ="L" />
+          <label htmlFor="L">L</label><br />
+          <input type="radio" id="XL" name="shirt size" value ="XL" />
+          <label htmlFor="XL">XL</label><br />
+        </form>
+        <form>Experience Level:
+          <input type="radio" id="beginner" name="experience" value ="beginner" />
+          <label htmlFor="beginner">Beginner</label><br />
+          <input type="radio" id="Intermediate" name="experience" value ="Intermediate" />
+          <label htmlFor="Intermediate">Intermediate</label><br />
+          <input type="radio" id="Expert" name="experience" value ="Expert" />
+          <label htmlFor="Expert">Expert</label><br />
+        </form>
         <button>REGISTER</button>
       </div>
     );
