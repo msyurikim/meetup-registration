db0ce42602f244a0f112ff5ff0121e130e896f7b
Jesse Beard <jjbeard@protonmail.com>
2020-05-18 09:26:55 -0700

    setup basic functionality of class

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 
 class App extends React.Component {
-// YOUR CODE HERE
+  constructor (props) {
+    super(props);
+    this.state = {
+      atendees: []
+    }
+  }
+
+
+
+  render(){
+    return (
+      <h2>New Attendee</h2>
+    )
+  }
 
 }
 
