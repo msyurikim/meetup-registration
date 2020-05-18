001cfc71b7f3b690a61a0cdfcfd5bcff720acf20
Imran Kheraj <ikheraj17@gmail.com>
2020-05-18 11:56:11 -0500

    Completed Axios get for attendees, sorted into categories within state

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RegisterForm from './RegisterForm.jsx';
 import AttendeeList from './AttendeeList.jsx';
+import axios from 'axios';
 
 
 class App extends React.Component {
@@ -10,9 +11,38 @@ constructor(props) {
         beginner: [],
         intermediate: [],
         expert: [],
+        all: [],
     }
 }
 
+getAttendees () {
+    axios.get('/attendees')
+      .then(response => {
+          let beginner = [];
+          let inter = [];
+          let expert = [];
+          for (var i = 0; i < response.data.length; i ++) {
+              let attendee = response.data[i];
+              if(attendee.skillLevel === "beginner") {
+                  beginner.push(attendee);
+              } else if (attendee.skillLevel === "intermediate") {
+                  inter.push(attendee);
+              } else {
+                  expert.push(attendee);
+              }
+              }
+              this.setState({
+                  beginner: beginner,
+                  intermediate: inter,
+                  expert: expert,
+              })
+          })
+      }
+
+
+componentDidMount() {
+    this.getAttendees();
+}
 
 
 render () {
