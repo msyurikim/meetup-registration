d3c26e1f02b8f372c0b1885f6aad78f509b4f4e4
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-18 13:33:16 -0400

    Assessment submission

--- meetup-front-end/src/components/Register.jsx
+++ meetup-front-end/src/components/Register.jsx
@@ -1,30 +1,62 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      shirt: '',
+      skillLevel: ''
     }
-    this.handleOnClick.bind(this);
+    this.handleChange.bind(this);
+    this.handleSubmit.bind(this);
   }
 
-  handleOnClick() {
-    console.log('clicked');
+  // NOTE: Was stuck on a typeerror for "Uncaught TypeError: Cannot read property 'state' of undefined" for about 35minutes...
+  handleChange(e) {
+    // console.log(e.target.name, e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { id, firstName, lastName, email, shirt, skillLevel } = this.state;
+    $.ajax({
+      method: 'POST',
+      url: '/attendees',
+      data: {
+        id,
+        firstName,
+        lastName,
+        email,
+        shirt,
+        skillLevel
+      }
+    })
+      .done(() => {
+        // invoke a method (passed from parent) to refresh attendees
+      })
   }
 
   render() {
+    const { firstName, lastName, email, shirt, skillLevel } = this.state;
     return (
-      <div className="attendee-form">
+      <form className="attendee-form" onSubmit={this.handleSubmit}>
         <h2>Register Attendee</h2>
         <label>First Name:
-          <input type="text"></input>
+          <input type="text" name="firstName" value={firstName} onChange={this.handleChange}></input>
         </label>
         <label>Last Name:
-          <input type="text"></input>
+          <input type="text" name="lastName" value={lastName} onChange={this.handleChange}></input>
         </label>
         <label>Email:
-          <input type="email"></input>
+          <input type="email" name="email" value={email} onChange={this.handleChange}></input>
         </label>
         <label>Shirt Size:
           <select>
@@ -45,8 +77,8 @@ class Register extends React.Component {
             <option value="Expert">Expert</option>
           </select>
         </label>
-        <button onClick={() => this.handleOnClick()}>REGISTER</button>
-      </div>
+        <input type="submit" value="REGISTER"></input>
+      </form>
     )
   }
 }
