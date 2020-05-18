315859f9697593316e9d10e31e5930585a7c1168
Hieu Ho <hihho@ucdavis.edu>
2020-05-18 09:18:12 -0700

    npm install, basic react page

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 
 class App extends React.Component {
 // YOUR CODE HERE
+  constructor(props) {
+    super(props);
 
+
+  }
+  render () {
+    return (
+      <div> hellow </div>
+    )
+  }
 }
 
 export default App;
