deed1fb232c4b715611db86eee381a694a4f7f72
joshskkim <josh.sk.kim@gmail.com>
2020-05-18 10:01:28 -0700

    Saves info on input

--- meetup-front-end/src/components/AttendeesForm.jsx
+++ meetup-front-end/src/components/AttendeesForm.jsx
@@ -4,25 +4,74 @@ class AttendeesForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      firstName: '',
+      lastName: '',
+      email: '',
+      shirt: '',
+      skillLevel: ''
     };
 
+    this.setFirstName = this.setFirstName.bind(this);
+    this.setLastName = this.setLastName.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setShirt = this.setShirt.bind(this);
+    this.setSkill = this.setSkill.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  setFirstName (e) {
+    let newFN = e.target.value;
+    this.setState({
+      firstName: newFN,
+    });
+  }
+
+  setLastName (e) {
+    let newLN = e.target.value;
+    this.setState({
+      lastName: newLN,
+    });
+  }
+
+  setEmail (e) {
+    let newEmail = e.target.value;
+    this.setState({
+      email: newEmail,
+    });
+  }
+
+  setShirt (e) {
+    let newShirt = e.target.value;
+    this.setState({
+      shirt: newShirt,
+    });
+  }
+
+  setSkill (e) {
+    let newSkill = e.target.value;
+    this.setState({
+      skillLevel: newSkill,
+    });
+  }
+
+  handleSubmit (e) {
+    console.log(e.target);
   }
 
   render () {
     return (
-      <div class="attendee-form">
+      <div className="attendee-form">
         <h2>Register Attendee</h2>
         <label>First Name:
-          <input type="text"></input>
+          <input type="text" onChange={this.setFirstName}></input>
         </label>
         <label>Last Name:
-          <input type="text"></input>
+          <input type="text" onChange={this.setLastName}></input>
         </label>
         <label>Email Address:
-          <input type="email"></input>
+          <input type="email" onChange={this.setEmail}></input>
         </label>
-        <form>Shirt Size:
+        <form onChange={this.setShirt}>Shirt Size:
           <input type="radio" id="XS" name="shirt size" value ="XS" />
           <label htmlFor="XS">XS</label><br />
           <input type="radio" id="S" name="shirt size" value ="S" />
@@ -34,7 +83,7 @@ class AttendeesForm extends React.Component {
           <input type="radio" id="XL" name="shirt size" value ="XL" />
           <label htmlFor="XL">XL</label><br />
         </form>
-        <form>Experience Level:
+        <form onChange={this.setSkill}>Experience Level:
           <input type="radio" id="beginner" name="experience" value ="beginner" />
           <label htmlFor="beginner">Beginner</label><br />
           <input type="radio" id="Intermediate" name="experience" value ="Intermediate" />
@@ -42,7 +91,7 @@ class AttendeesForm extends React.Component {
           <input type="radio" id="Expert" name="experience" value ="Expert" />
           <label htmlFor="Expert">Expert</label><br />
         </form>
-        <button>REGISTER</button>
+        <button onClick={this.handleSubmit}>REGISTER</button>
       </div>
     );
   }
