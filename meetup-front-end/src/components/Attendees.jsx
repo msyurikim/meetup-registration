04021c1822f2112ef1b430d9646b874da1758c70
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-18 12:25:34 -0400

    Move code from index into separate components

---
+++ meetup-front-end/src/components/Attendees.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Attendees(props) {
+  return (
+    <div class="attendees">
+    <h2>Attendees</h2>
+    <h3>Beginner</h3>
+    <div>Hoban Washburn</div>
+    <div>Jayne Cobb</div>
+    <h3>Intermediate</h3>
+    <div>Zoe Washburn</div>
+    <h3>Expert</h3>
+    <div>Malcom Reyolds</div>
+    <div>Kaylee Frye</div>
+  </div>
+  )
+}
+
+export default Attendees;
---
+++ meetup-front-end/src/components/Register.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div class="attendee-form">
+        <h2>Register Attendee</h2>
+        <label>First Name:
+          <input type="text"></input>
+        </label>
+        <label>Last Name:
+          <input type="text"></input>
+        </label>
+        <button>REGISTER</button>
+      </div>
+    )
+  }
+}
+
+export default Register;
