dbf640284977e809e69a3ed2d7fe05808de77a97
joshskkim <josh.sk.kim@gmail.com>
2020-05-18 09:31:08 -0700

    Maps all attendees

--- meetup-front-end/src/components/AttendeesList.jsx
+++ meetup-front-end/src/components/AttendeesList.jsx
@@ -3,11 +3,20 @@ import Attendee from 'Attendee.jsx'
 
 const AttendeesList = ({attendees}) => {
   if(attendees) {
-
+    return (
+      <div className="attendees">
+        <h2>Attendees</h2>
+        {attendees.map(attendee => {
+          return (
+            <Attendee key={attendee.id} attendee={attendee} />
+          );
+        })}
+      </div>
+    );
   } else {
     return (
       <div>Loading Attendees...</div>
-    )
+    );
   }
 };
 
