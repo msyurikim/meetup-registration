704c22e10e80622639e85a094944ab09045bdcae
joshskkim <josh.sk.kim@gmail.com>
2020-05-18 09:42:54 -0700

    Added options for attendee form

--- meetup-front-end/src/components/AttendeesForm.jsx
+++ meetup-front-end/src/components/AttendeesForm.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 
-const AttendeesForm = () => {
-  return (
-    <div class="attendee-form">
-      <h2>Register Attendee</h2>
-      <label>First Name:
-        <input type="text"></input>
-      </label>
-      <label>Last Name:
-        <input type="text"></input>
-      </label>
-      <button>REGISTER</button>
-    </div>
-  );
+class AttendeesForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+  }
+
+  render () {
+    return (
+      <div class="attendee-form">
+        <h2>Register Attendee</h2>
+        <label>First Name:
+          <input type="text"></input>
+        </label>
+        <label>Last Name:
+          <input type="text"></input>
+        </label>
+        <label>Email Address:
+          <input type="text"></input>
+        </label>
+        <label>Shirt Size:
+          <input type="text"></input>
+        </label>
+        <label>Experience Level:
+          <input type="text"></input>
+        </label>
+        <button>REGISTER</button>
+      </div>
+    );
+  }
 };
 
 export default AttendeesForm;
