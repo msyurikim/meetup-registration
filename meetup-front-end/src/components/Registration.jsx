9adea76a3c7551f2467f4190c77b79078ef422d8
melvinioso <matt.mcgavern@gmail.com>
2020-05-18 12:17:23 -0500

    Added Registration component

---
+++ meetup-front-end/src/components/Registration.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      shirt: '',
+      skillLevel: '',
+    };
+
+
+}
