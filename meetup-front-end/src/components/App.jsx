ab71ae436fb46de29ebb0b0f852cb1e18253ad01
Adam Tiller <a.j.tiller@gmail.com>
2020-05-18 09:28:36 -0700

    Fetch attendees from server on componentDidMount.

--- meetup-front-end/src/components/App.jsx
+++ meetup-front-end/src/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import axios from 'axios';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+  }
+
+  getAttendees() {
+    axios.get('/attendees')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.getAttendees();
   }
 
 
